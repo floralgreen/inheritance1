59b9ac8b15bb632a013e95b1747b152129f60304
floralgreen <carducci.federico20@gmail.com>
2023-12-05 10:32:52 +0100

    Esercizio completato

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animale animale1 = new Animale();
+        Gatto gatto1 = new Gatto();
+
+        System.out.print("Verso animale: ");
+        animale1.faiIlVerso();
+
+        System.out.print("Verso gatto: ");
+        gatto1.faiIlVerso();
+
+    }
+
+}
